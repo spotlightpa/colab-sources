47aa8b2da69e3e67e4e97985c5ca3817da935e11
Carl Johnson <me@carlmjohnson.net>
2022-01-27 12:51:11 -0500

    Search: Use CTMirror settings

--- assets/js/utils/search-api.js
+++ assets/js/utils/search-api.js
@@ -1,8 +1,8 @@
 import fetchJSON from "./fetch-json.js";
 
-const appID = `5M1ASV9W0A`;
-const publicKey = `fd9492397caaffd9cb49be210170e63a`;
-const indexName = `spotlightpa-sourcesdb`;
+const appID = `4YY8001LKK`;
+const publicKey = `7bbfad285bdadbfe564a6b2a0dcafc79`;
+const indexName = `ctmirrror-sources`;
 
 let baseURL = `https://${appID}-dsn.algolia.net/1/indexes/${indexName}?x-algolia-agent=spotlightpa&x-algolia-application-id=${appID}&x-algolia-api-key=${publicKey}&hitsPerPage=${999}&query=`;
 
